16d61e412ceaa560a13d668dd135f871444a9d20
nismail1 <nimo_ismail@brown.edu>
2024-05-05 18:00:03 -0400

    in the middle of creating the books view

--- library/library/catalog/urls.py
+++ library/library/catalog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+]
+urlpatterns = [
+    path('books/', views.BookListView.as_view(), name='index'),
 ]
